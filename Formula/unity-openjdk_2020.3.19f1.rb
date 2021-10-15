179ca6ca9787dbeab414318598b1ed4f283c79e6
Brandon Boles <bboles@redemptiongames.com>
2021-10-15 10:50:24 -0700

    Initial commit

---
+++ Formula/unity-openjdk_2020.3.19f1.rb
@@ -0,0 +1,14 @@
+class Unity-OpenJDK_2020.3.19f1 < Formula
+  desc "OpenJDK for Unity"
+  homepage "https://unity.com/products"
+  url "http://download.unity3d.com/download_unity/open-jdk/open-jdk-mac-x64/jdk8u172-b11_4be8440cc514099cfe1b50cbc74128f6955cd90fd5afe15ea7be60f832de67b4.zip"
+  sha256 "01c8518f34d083b6adf57f1a3a7cff76e78dc0a2523b6332d10a0f30fcb8d308"
+
+  depends_on cask: unity-android_2020.3.19f1
+
+  def install
+    Dir.mkdir '/Applications/Unity.2020.3.19f1/PlaybackEngines/AndroidPlayer/OpenJDK'
+    system "unzip", "jdk8u172-b11_4be8440cc514099cfe1b50cbc74128f6955cd90fd5afe15ea7be60f832de67b4.zip", "-d", "/Applications/Unity.2020.3.19f1/PlaybackEngines/AndroidPlayer/OpenJDK"
+  end
+
+end
