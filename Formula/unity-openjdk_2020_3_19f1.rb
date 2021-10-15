afbd805c84741e20e85a9c4af055608cdd9d20a7
Brandon Boles <bboles@redemptiongames.com>
2021-10-15 11:13:50 -0700

    Remove 'cask'

--- Formula/unity-openjdk_2020_3_19f1.rb
+++ Formula/unity-openjdk_2020_3_19f1.rb
@@ -4,7 +4,7 @@ class UnityOpenJDK_2020_3_19f1 < Formula
   url "http://download.unity3d.com/download_unity/open-jdk/open-jdk-mac-x64/jdk8u172-b11_4be8440cc514099cfe1b50cbc74128f6955cd90fd5afe15ea7be60f832de67b4.zip"
   sha256 "01c8518f34d083b6adf57f1a3a7cff76e78dc0a2523b6332d10a0f30fcb8d308"
 
-  depends_on cask: 'unity-android_2020.3.19f1'
+  depends_on 'unity-android_2020.3.19f1'
 
   def install
     Dir.mkdir '/Applications/Unity.2020.3.19f1/PlaybackEngines/AndroidPlayer/OpenJDK'
