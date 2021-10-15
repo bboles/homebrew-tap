00cb87803c67eb561fcf020231c754738413a47f
Brandon Boles <bboles@redemptiongames.com>
2021-10-14 17:20:39 -0700

    Initial commit

---
+++ Formula/unity_2020.3.19f1.rb
@@ -0,0 +1,25 @@
+cask "unity_2020.3.19f1" do
+  version "2020.3.19f1,668f137dc9bbe"
+  sha256 "ce8b769aea39c8e3b38afa2e484fccf8de8878c1925eeae96e60b9fa6422805e"
+
+  url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg",
+      verified: "download.unity3d.com/download_unity/"
+  name "Unity Editor"
+  desc "Platform for 3D content"
+  homepage "https://unity.com/products"
+
+  livecheck do
+    url "https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json"
+    strategy :page_match do |page|
+      page.scan(%r{/download_unity/(\h+)/MacEditorInstaller/Unity-(\d+(?:\.\d+)*[a-z]*\d*)\.pkg}i).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
+
+  pkg "Unity-#{version.before_comma}.pkg"
+
+  uninstall quit:    "com.unity3d.UnityEditor5.x",
+            pkgutil: "com.unity3d.UnityEditor5.x",
+            delete:  "/Applications/Unity"
+end
