f8b924627543657c7623cdccf24aafd6af90d2e3
Brandon Boles <bboles@redemptiongames.com>
2021-10-18 12:21:45 -0700

    Fix cask

--- Casks/unity-android-sdkplatformtools_2020.3.18f1.rb
+++ Casks/unity-android-sdkplatformtools_2020.3.18f1.rb
@@ -1,26 +1,22 @@
-cask "unity-android-sdkplatforms_2020.3.18f1" do
+cask "unity-android-sdkplatformtools_2020.3.18f1" do
   version "2020.3.18f1"
-  sha256 "8452dbbf9668a428abb243c4f02a943b7aa83af3cca627629a15c4c09f28e7bd"
+  sha256 "3bc833ae3f4bd831af03811f2d1be540c2eb2eb9a17de9398b0a06dc5af6fa84"
 
-  _platforms_version = '28_r06'
-  url "https://dl.google.com/android/repository/platform-#{_platforms_version}.zip"
-  name "Android SDK Build Tools #{_platforms_version}"
-  desc "Android SDK Build Tools for use specifically with Unity"
+  _platformtools_version = '28.0.1'
+  url "https://dl.google.com/android/repository/platform-tools_r#{_platformtools_version}-darwin.zip"
+  name "Android SDK Platform Tools #{_platformtools_version}"
+  desc "Android SDK Platform Tools for use specifically with Unity"
   homepage "https://unity.com/products"
 
-  _platforms_path = "/Applications/Unity.#{version}/PlaybackEngines/AndroidPlayer/SDK/platforms/android-28"
+  _platformtools_path = "/Applications/Unity.#{version}/PlaybackEngines/AndroidPlayer/SDK"
   installer script: {
     executable: "/bin/cp",
-    args:       ["-pr", "#{caskroom_path}/#{version}/android-9/", "#{_platforms_path}"],
+    args:       ["-pr", "#{caskroom_path}/#{version}/", "#{_platformtools_path}"],
     sudo:       true,
   }
 
-  preflight do
-    system '/usr/bin/sudo', '-E', '--', 'mkdir', '-p', "#{_platforms_path}"
-  end
-
   postflight do
-    set_ownership("#{_platforms_path}", user: 'root', group: 'wheel')
+    set_ownership("#{_platformtools_path}", user: 'root', group: 'wheel')
   end
 
   depends_on cask: "unity-android-sdkndktools_#{version.before_comma}"
